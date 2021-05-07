dcb175ab0cda9fbfd5a8fe2f22c41f8e21996830
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-05-07 14:01:05 +0000

    Initial commit

--- warbler-client/src/components/AuthForm.js
+++ warbler-client/src/components/AuthForm.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 
 class AuthForm extends Component {
   constructor(props) {
@@ -15,14 +14,9 @@ class AuthForm extends Component {
   handleSubmit = e => {
     e.preventDefault();
     const authType = this.props.signUp ? "signup" : "signin";
-    this.props
-      .onAuth(authType, this.state)
-      .then(() => {
-        this.props.history.push("/");
-      })
-      .catch(() => {
-        return;
-      });
+    this.props.onAuth(authType, this.state).then(() => {
+      console.log("LOGGED IN!");
+    });
   };
 
   handleChange = e => {
@@ -31,18 +25,7 @@ class AuthForm extends Component {
 
   render() {
     const { email, username, password, profileImageUrl } = this.state;
-    const {
-      signUp,
-      heading,
-      buttonText,
-      errors,
-      history,
-      removeError
-    } = this.props;
-
-    history.listen(() => {
-      removeError();
-    });
+    const { signUp, heading, buttonText } = this.props;
 
     return (
       <div>
@@ -50,9 +33,7 @@ class AuthForm extends Component {
           <div className="col-md-6">
             <form onSubmit={this.handleSubmit}>
               <h2>{heading}</h2>
-              {errors.message && (
-                <div className="alert alert-danger">{errors.message}</div>
-              )}
+
               <label htmlFor="email">E-mail</label>
               <input
                 autoComplete="off"
@@ -110,14 +91,5 @@ class AuthForm extends Component {
     );
   }
 }
-AuthForm.propTypes = {
-  buttonText: PropTypes.string,
-  errors: PropTypes.object,
-  heading: PropTypes.string,
-  history: PropTypes.object,
-  onAuth: PropTypes.func,
-  signIn: PropTypes.bool,
-  removeError: PropTypes.func
-};
 
 export default AuthForm;
--- warbler-client/src/containers/Main.js
+++ warbler-client/src/containers/Main.js
@@ -4,28 +4,19 @@ import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
 import AuthForm from "../components/AuthForm";
 import { authUser } from "../store/actions/auth";
-import { removeError } from "../store/actions/errors";
-import withAuth from "../hocs/withAuth";
-import MessageForm from "../containers/MessageForm";
 
 const Main = props => {
-  const { authUser, errors, removeError, currentUser } = props;
+  const { authUser } = props;
   return (
     <div className="container">
       <Switch>
-        <Route
-          exact
-          path="/"
-          render={props => <Homepage currentUser={currentUser} {...props} />}
-        />
+        <Route exact path="/" render={props => <Homepage {...props} />} />
         <Route
           exact
           path="/signin"
           render={props => {
             return (
               <AuthForm
-                removeError={removeError}
-                errors={errors}
                 onAuth={authUser}
                 buttonText="Log in"
                 heading="Welcome Back."
@@ -40,8 +31,6 @@ const Main = props => {
           render={props => {
             return (
               <AuthForm
-                removeError={removeError}
-                errors={errors}
                 onAuth={authUser}
                 signUp
                 buttonText="Sign me up!"
@@ -51,10 +40,6 @@ const Main = props => {
             );
           }}
         />
-        <Route
-          path="/users/:id/messages/new"
-          component={withAuth(MessageForm)}
-        />
       </Switch>
     </div>
   );
@@ -62,11 +47,8 @@ const Main = props => {
 
 function mapStateToProps(state) {
   return {
-    currentUser: state.currentUser,
     errors: state.errors
   };
 }
 
-export default withRouter(
-  connect(mapStateToProps, { authUser, removeError })(Main)
-);
+export default withRouter(connect(mapStateToProps, { authUser })(Main));
