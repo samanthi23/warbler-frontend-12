3fa5550f055974a553c3dbd7ec84892c2b70f8ad
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-09-26 14:52:58 +0000

    api.js apiCall

--- warbler-client/src/services/api.js
+++ warbler-client/src/services/api.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
+// import axios from "axios";
+
 export function apiCall(method, path, data) {
   // returns a new promise
   // resolve promise when our actions resolved
   // if it doesn't go well then return an error .then .catch
   // {} an object called response and a sub object called data
   // import to store/actions/auth.js
+  // new Promise accepts a function
   return new Promise((resolve, reject) => {
     return axios[method](path, data)
       .then(res => {
+        // with a successful request we always get a sub object of data, res.data
         return resolve(res.data);
       })
       .catch(err => {
